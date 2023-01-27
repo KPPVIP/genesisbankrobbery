f3ba5a324e2a6d4b916c32cd51d86273c3f325fb
Guudz <Kilian1702@outlook.com>
2023-01-27 01:32:00 +0100

    Initial commit

---
+++ genesis-bankHoldup/locales/pl.lua
@@ -0,0 +1,7 @@
+Locales['pl'] = {
+  ['error_robbed'] = '~r~Ktoś już wcześniej obrabował ten skarbiec!',
+  ['error_notmanycops'] = '~b~Nie ma wystarczającej liczby funkcjonariuszy LSPD aby, rozpocząć napad.',
+  ['cops_notif'] = '~b~Napad na bank centralny ~r~rozpoczęty!',
+  ['cops_notif2'] = '~b~Napad na bank centralny ~r~zakończony!',
+  ['kasa'] = '~b~Podnosisz ~r~%s$ ~b~brudnej gotówki!',
+}
